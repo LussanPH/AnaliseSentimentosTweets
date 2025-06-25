ae2136f971c51f87769c22d1216bebb83acd9afc
Pedro Luna <166248501+LussanPH@users.noreply.github.com>
2025-06-25 11:18:52 -0300

    Alterações no comportamento do grid
    Adicionar a função adicionarPessoa

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import random as rd
 import numpy as np
 class Agente:
-    def __init__(self, id, xy, estado, tam):
+    def __init__(self, id, xy, estado):
         self.id = id
         self.x, self.y = xy
         self.estado = estado
-        self.tam = tam
 
     def getXy(self):
         return [self.x, self.y]    
+    
+    def getId(self):
+        return self.id
+    
+    def getEstado(self):
+        return self.estado
 
     def mover(self):
         if(self.x == 0 and self.y == 0):
@@ -61,13 +66,12 @@ class Ambiente:
     def __init__(self, quan, tam, quanInicial, PorcenInfec, quanDias):
         self.quan = quan
         self.tam = tam
-        self.grid = np.zeros((tam, tam))
         self.quanInicial = quanInicial
         self.PorcenInfec = PorcenInfec
         self.quanDias = quanDias
         self.listaAgentes = []
         
-    def gerarAgentes(self):
+    def gerarAgentes(self, grid):
         estado = 'S'
         id = 1
         x = 0
@@ -76,32 +80,59 @@ class Ambiente:
             if(self.quanInicial != 0):
                 estado = 'I'
                 self.quanInicial -= 1
-            else:
-                estado = 'S'
+
             x = rd.randint(0, self.tam-1)
             y = rd.randint(0, self.tam-1)
-            a = Agente(id, [x, y], estado, self.tam)
-            self.grid[y, x] += 1
-            self.listaAgentes.append(a)
+            a = Agente(id, [x, y], estado)
+            grid[y][x].append(a.getId())
             id += 1
 
-    def printarGrid(self):
-        for linha in self.grid:
-            print(linha)
-        print("\n")
+    def gerarGrid(self):
+        grid = []
+        for i in range(self.tam):
+            listai = []
+            grid.append(listai)
+            for j in range(self.tam):
+                listaj = []
+                listai.append(listaj)
+        return grid        
 
+    def printarGrid(self, grid):
+        for linha in grid:
+            print(linha)
+        print("\n")    
+
+    def removerPessoa(self, valoresXy, id, grid):
+        for a in grid[valoresXy[1], valoresXy[0]]:
+            idAtual = a.getId()
+            if idAtual == id:
+                grid[valoresXy[1], valoresXy[0]].remove(a)
+                return
+            
+    def adicionarPessoa(self, valoresXy) #Falta adicionar pessoa       
+            
 
     def simulacao(self):
-        self.gerarAgentes()
-        self.printarGrid()
+        grid = self.gerarGrid()
+        self.gerarAgentes(grid)
+        self.printarGrid(grid)
 
         for d in range(self.quanDias):
+
             for a in self.listaAgentes:
+
                 valoresXY = a.getXy()
-                self.grid[valoresXY[1], valoresXY[0]] -= 1
+                estado = a.getEstado()
+                id = a.getId()
+
+                #if(pessoas > 1 and estado == 'I'):
+                    #self.infectar(pessoas, self.listaAgentes)
+
+                self.removerPessoa(valoresXY, id, grid)
                 a.mover()
                 valoresXY = a.getXy()
-                self.grid[valoresXY[1], valoresXY[0]] += 1
+                grid[valoresXY[1], valoresXY[0]].append(a)
+
             self.printarGrid()    
 
         self.printarGrid()   
@@ -112,7 +143,6 @@ class Ambiente:
 
 amb = Ambiente(10, 5, 2, 0.1, 10)
 amb.simulacao()
-
 
 
 
